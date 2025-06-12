035d53bf881857b3a720db19843b5751801b2c38
Yonathan canales <83363899+Sopapo3k@users.noreply.github.com>
2025-06-12 07:41:59 -0400

    agrega carpeta back
    joni gei

---
+++ libraryback/src/main/java/cl/ucm/libraryback/LibrarybackApplication.java
@@ -0,0 +1,13 @@
+package cl.ucm.libraryback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarybackApplication.class, args);
+	}
+
+}
